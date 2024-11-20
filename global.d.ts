ddf2c109ae907e6fe87a003eca12fc65b85baa07
anuj sachan <anuj.sachan@akqa.com>
2024-11-20 16:45:19 +1100

    added vercel analytics

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,6 @@
 interface Window {
   analytics: {
+    //
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     track: (event: string, properties?: Record<string, any>) => void;
   };
